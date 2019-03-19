6a6ceff2b8c0c04ada40107d1c92038ea190250c
Aaron Young <ayoung3210@gmail.com>
2019-03-18 23:14:31 -0400

    Added logo line also.
    The images do not work properly with feeder.co but they do work
    correctly when tested with liferea.

--- update_feed_gem.sh
+++ update_feed_gem.sh
@@ -8,11 +8,20 @@ FEED_FILE=~/gems/gems/jekyll-feed-0.11.0/lib/jekyll-feed/feed.xml
 
 # Add icon line to jekyll-feed template file.
 if [ -f $FEED_FILE ]; then
+   # Add Icon Line
    if ! grep -q "<icon>{{ '/assets/favicons/favicon.ico' | absolute_url }}</icon>" $FEED_FILE ; then
       sed -i "11i\  <icon>{{ '/assets/favicons/favicon.ico' | absolute_url }}</icon>" $FEED_FILE
       echo "Added \"<icon>{{ '/assets/favicons/favicon.ico' | absolute_url }}</icon>\" to $FEED_FILE"
    else
       echo "Icon string already in gem."
+   fi
+
+   # Add Logo Line
+   if ! grep -q "<logo>{{ '/assets/images/logo88x88.png' | absolute_url }}</logo>" $FEED_FILE ; then
+      sed -i "12i\  <logo>{{ '/assets/images/logo88x88.png' | absolute_url }}</logo>" $FEED_FILE
+      echo "Added \"<logo>{{ '/assets/images/logo88x88.png' | absolute_url }}</logo>\" to $FEED_FILE"
+   else
+      echo "Logo string already in gem."
    fi
 else
    echo "Could not find the jekyll-feed template file at $FEED_FILE."
