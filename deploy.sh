eccf1aad4f25bdb96fc113b1fbfd4214dc6101a3
Aaron Young <ayoung3210@gmail.com>
2021-02-21 22:29:24 -0500

    Added new deploy scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
-# Script to deploy the website to hydra.
+# Script to deploy the website to Github.
 #-------------------------------------------------------------------------------
-
-# Variables
-instancehost="hydra10.eecs.utk.edu"
-remotewebroot="www-home"
 
 # Build Jekyll Site
 JEKYLL_ENV=production bundle exec jekyll build
 chmod -R a+rX _site
 
-echo "rsync to SSH host $instancehost ..."
-
-# Long form of rsync options
-# rsync --verbose --recursive --compress --checksum --human-readable --perms --delete-after
-rsync -vrzchp --delete-after _site/ $instancehost:$remotewebroot
+echo 'Deploy to Github using deploy_github.sh'
+./deploy_github.sh
