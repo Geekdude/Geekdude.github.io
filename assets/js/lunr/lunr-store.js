7bd83933e2dc806a703e2f3c5b9795b6f19aed0c
Aaron Young <ayoung3210@gmail.com>
2021-10-31 13:26:25 -0400

    publish: Added post about utterances change.
    generated from commit 291d165530d5ae0f2829486a5dcdf6e779e59d43

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -106,4 +106,10 @@ var store = [{
         "tags": ["News"],
         "url": "/tech/dean-nairr/",
         "teaser": "/assets/images/dean-nairr-teaser.png"
+      },{
+        "title": "Changing from Disqus to Utterances",
+        "excerpt":"I have been having an issue where Disqus would not send me any emails when new comments are posted to the website. After going through all the settings in Disqus, changing my email address, and testing adding comments using a guest login, I could not figure out how to get email notifications to work. Because of this, I changed to using Utterances for my website’s comments. Utterances’ is a great choice since it is open-source and uses the issues tracker of the GitHub repository to store the comments. Each page is its own issue, and the comments are added to the issue. Now email notification works just like GitHub’s issue email notification system. The main downside to Utterances is that you have to log in to a GitHub account in order to post a comment. I liked the flexibility that Disqus provided in terms of login types and guest posting, but I need a commenting system that notifies me when new comments are added, and I like that comments are stored as issues in the issue tracker.  ","categories": ["Tech"],
+        "tags": ["Website"],
+        "url": "/tech/changing-to-utterances/",
+        "teaser": "/assets/images/utterances-teaser.png"
       }]
